3fee27f90bffb7d4d36c176359fcf877c9366c76
Carlos Lenon <code@carloslenon.com>
2019-01-15 15:49:36 -0600

    add redux logger and dev tools for windows

--- src/common/store/index.js
+++ src/common/store/index.js
@@ -6,14 +6,21 @@
  * @author @clenondavis <dev@carloslenon.com>
  * @return {Object} List of store reducers
  */
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import appReducers from '../reducers';
 
-const store = createStore(appReducers, applyMiddleware(thunk));
+const logger = createLogger();
 
-store.subscribe(() => {
-  console.log('next state', store.getState());
-});
+const store = createStore(
+  appReducers,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f,
+  ),
+);
 
 export default store;
